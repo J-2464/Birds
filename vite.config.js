647a0efeb231750455d691a2eb01aa5952c7c758
J-2464 <jamesgsaba@gmail.com>
2025-03-12 17:21:27 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: '/your-repository-name/',  // Replace with your actual GitHub repository name
+  base: '/Birds/',  // Replace with your actual GitHub repository name
 });
